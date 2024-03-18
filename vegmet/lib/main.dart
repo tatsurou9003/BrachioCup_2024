45b752fefb5d8dec1f373042f837d31485c32bc4
tatsurou9003 <t.murakami@4nonome.com>
2024-03-18 12:14:40 +0900

    amplify setup

--- vegmet/lib/main.dart
+++ vegmet/lib/main.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 
+import 'amplifyconfiguration.dart';
 import 'footer.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    try {
+      await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.configure(amplifyconfig);
+      print('Successfully configured');
+    } on Exception catch (e) {
+      print('Error configuring Amplify: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'VEGMET',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Veggie Gourmet'),
-      debugShowCheckedModeBanner: false, 
-    );
+    return Authenticator(
+        initialStep: AuthenticatorStep.signIn,
+        child: MaterialApp(
+          builder: Authenticator.builder(),
+          title: 'VEGMET',
+          home: const MyHomePage(title: 'VEGMET'),
+        ));
   }
 }
 
@@ -33,7 +56,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
